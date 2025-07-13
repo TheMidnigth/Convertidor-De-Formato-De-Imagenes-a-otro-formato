7f6ec3f31dde33c8d26bcf08fd9a57d7e219d67e
TheMidnight <reyeskeyner110@gmail.com>
2025-07-13 17:19:47 -0500

    Initial commit

---
+++ src/main/java/com/ProyectConversionImg/ConversionImg/ConversionImgApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectConversionImg.ConversionImg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionImgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionImgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectConversionImg/ConversionImg/ConversionImgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectConversionImg.ConversionImg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionImgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
