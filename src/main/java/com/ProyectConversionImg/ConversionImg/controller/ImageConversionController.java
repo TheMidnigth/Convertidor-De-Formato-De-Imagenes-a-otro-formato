b2461d6d627724c946304231ef2693a1491cc8cb
TheMidnight <reyeskeyner110@gmail.com>
2025-07-13 18:11:05 -0500

    Agregando la clase controller

---
+++ src/main/java/com/ProyectConversionImg/ConversionImg/controller/ImageConversionController.java
@@ -0,0 +1,11 @@
+package com.ProyectConversionImg.ConversionImg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/images")
+public class ImageConversionController {
+
+
+}
