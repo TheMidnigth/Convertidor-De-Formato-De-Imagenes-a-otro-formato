24a760793266c7811c10f58d6677eccd2734fc9f
keyner reyes <reyeskeyner110@gmail.com>
2025-07-18 17:09:00 -0500

    Add Conversiones and FAQ sections with accordion
    Introduces new 'Conversiones Populares' and 'Preguntas Frecuentes' sections to the homepage, including a list of popular image conversions and an FAQ accordion. Updates navigation links, adds Bricolage Grotesque font, and enhances styles for new sections and accordion functionality. Improves JS to support accordion behavior and slider cloning.

--- src/main/resources/Static/js/main.js
+++ src/main/resources/Static/js/main.js
@@ -1,2 +1,52 @@
-const logos = document.getElementById("slider").cloneNode(true);
-document.getElementById("logos").appendChild(logos);
+window.addEventListener('DOMContentLoaded', () => {
+
+    const logosContainer = document.getElementById("logos");
+
+    // Elimina cualquier clon previo si existe
+    const clonPrevio = logosContainer.querySelector('.slider-clone');
+    if (clonPrevio) {
+        clonPrevio.remove();
+    }
+
+    // Clonamos de nuevo el slider original
+    const logos = document.getElementById("slider").cloneNode(true);
+    logos.classList.add('slider-clone');
+    logosContainer.appendChild(logos);
+
+    //Acordion javascript
+    const accordionButtons = document.querySelectorAll('.accordion__button');
+
+    accordionButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const content = button.nextElementSibling;
+            const icon = button.querySelector('.accordion__icon');
+
+            // Cerrar todos los demás
+            document.querySelectorAll('.accordion__content').forEach(c => {
+                if (c !== content) c.classList.remove('open');
+            });
+
+            document.querySelectorAll('.accordion__icon').forEach(i => {
+                if (i !== icon) {
+                    i.classList.remove('fa-minus');
+                    i.classList.add('fa-plus');
+                }
+            });
+
+            // Alternar el actual
+            if (content.classList.contains('open')) {
+                content.classList.remove('open');
+                icon.classList.remove('fa-minus');
+                icon.classList.add('fa-plus');
+            } else {
+                content.classList.add('open');
+                icon.classList.remove('fa-plus');
+                icon.classList.add('fa-minus');
+            }
+        });
+    });
+
+
+
+});
+
